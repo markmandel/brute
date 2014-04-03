1d9e306efb1dac08b4d4d44c3bee7dd0174ebbd0
Mark Mandel <mark.mandel@gmail.com>
2014-04-03 16:44:54 +1100

    Lots of entity and component manipulation functions.

--- src/brute/core.clj
+++ src/brute/core.clj
@@ -1,21 +1,51 @@
 (ns ^{:doc "Core API for the Brute Entity Component System"}
     brute.core)
 
-(def ^{:private true} all-entities (atom []))
+(def ^{:private true} all-entities (ref []))
+(def ^{:private true} entity-components (ref {}))
 
 (defn reset-all!
     "Resets the state of this entity component system. Good for tests"
     []
-    (alter-var-root #'all-entities (constantly (atom []))))
+    (alter-var-root #'all-entities (constantly (ref [])))
+    (alter-var-root #'entity-components (constantly (ref {}))))
 
 (defn create-entity!
     "Creates an entity and stores it"
     []
-    (let [uuid (java.util.UUID/randomUUID)]
-        (swap! all-entities conj uuid)
-        uuid))
+    (let [entity (java.util.UUID/randomUUID)]
+        (dosync
+            (alter all-entities conj entity))
+        entity))
 
 (defn get-all-entities
     "Returns all the entities. Not that useful in application, but good for debugging/testing"
     []
     @all-entities)
+
+(defmulti get-component-type
+          "Returns the type for a given component. Using a multimethod with 'class' as the dispatch-fn to allow for extensibility per application.
+          By default returns the class of the component."
+          class)
+
+(defmethod get-component-type :default
+           [component]
+    (class component))
+
+(defn add-component!
+    "Add a component instance to a given entity"
+    [entity instance]
+    (dosync
+        (alter entity-components assoc-in [(get-component-type instance) entity] instance)))
+
+(defn get-component
+    "Get the component data for a specific component type"
+    [entity type]
+    (get-in @entity-components [type entity]))
+
+(defn get-all-entities-with-component
+    "Get all the entities that have a given component type"
+    [type]
+    (if-let [entities (keys (get @entity-components type))]
+        entities
+        []))
--- test/brute/core_test.clj
+++ test/brute/core_test.clj
@@ -10,6 +10,9 @@
 
 (namespace-state-changes (before :facts (setup!)))
 
+(defrecord Position [x y])
+(defrecord Velocity [x y])
+
 (fact "The Entity I create is a unique uuid"
       (let [uuid (create-entity!)]
           uuid => truthy
@@ -20,3 +23,29 @@
 (fact "Creating an entity results in it being added to the global list"
       (let [uuid (create-entity!)]
           [uuid] => (get-all-entities)))
+
+(fact "By default, a component returns it's class as it's type"
+      (let [pos (->Position 5 5)]
+          (get-component-type pos) => (class pos)))
+
+(fact "You can add a component instance to an entity, and then retrieve it again"
+      (let [entity (create-entity!)
+            pos (->Position 5 5)]
+          (add-component! entity pos)
+          (get-component entity Position) => pos))
+
+(fact "If an entity doesn't have a component, it should return nil"
+      (let [entity (create-entity!)
+            pos (->Position 5 5)]
+          (get-component entity Position) => falsey
+          (add-component! entity pos)
+          (get-component entity Velocity) => falsey))
+
+(fact "Can retrieve all entites that have a single type"
+      (get-all-entities-with-component Position) => []
+      (let [entity1 (create-entity!)
+            entity2 (create-entity!)
+            pos (->Position 5 5)]
+          (add-component! entity1 pos)
+          (add-component! entity2 pos)
+          (get-all-entities-with-component Position) => (just #{entity1, entity2})))
