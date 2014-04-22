5a04ccbce8f7ee1cc9cdc9810440b4975b94ef69
Mark Mandel <mark.mandel@gmail.com>
2014-04-22 19:32:39 +1000

    Cleanup commented out code.

--- src/brute/entity.clj
+++ src/brute/entity.clj
@@ -30,16 +30,6 @@
             (assoc! :entity-component-types (-> system :entity-component-types (assoc entity #{})))
             persistent!)))
 
-#_
-(defn create-entity!
-    "Creates an entity and returns it. An entity is just an UUID"
-    []
-    (let (java.util.UUID/randomUUID)
-        (dosync
-            (alter all-entities conj entity)
-            (alter entity-component-types assoc entity #{}))
-        entity))
-
 (defmulti get-component-type
           "Returns the type for a given component. Using a multimethod with 'class' as the dispatch-fn to allow for extensibility per application.
           By default returns the class of the component."
@@ -62,25 +52,10 @@
             (assoc! :entity-component-types (assoc ects entity (-> ects (get entity) (conj type))))
             persistent!)))
 
-#_
-(defn add-component!
-    "Add a component instance to a given entity. Will overwrite a component if already set."
-    [entity instance]
-    (dosync
-        (let [type (get-component-type instance)]
-            (alter entity-components assoc-in [type entity] instance)
-            (alter entity-component-types assoc entity (conj (get @entity-component-types entity) type)))))
-
 (defn get-component
     "Get the component data for a specific component type"
     [system entity type]
     (-> system :entity-components (get-in [type entity])))
-
-#_
-(defn get-component
-    "Get the component data for a specific component type"
-    [entity type]
-    (get-in @entity-components [type entity]))
 
 (defn get-all-entities-with-component
     "Get all the entities that have a given component type"
@@ -101,20 +76,6 @@
             (assoc! :entity-component-types (assoc entity-component-types entity (-> entity-component-types (get entity) (disj type))))
             persistent!)))
 
-#_
-(defn remove-component!
-    "Remove a component instance from an entity"
-    [entity instance]
-    (let [type (get-component-type instance)]
-        (dosync
-            (alter entity-components assoc type (dissoc (get @entity-components type) entity))
-            (alter entity-component-types assoc entity (disj (get @entity-component-types entity) type)))))
-
-#_
-(reduce (fn [v e]
-            (assoc v e (dissoc (get v e) :e)))
-        m k)
-
 (defn kill-entity
     "Destroy an entity completely."
     [system entity]
@@ -126,17 +87,6 @@
             (assoc! :entity-components (persistent! (reduce (fn [v type] (assoc! v type (dissoc (get v type) entity)))
                                                             (transient (:entity-components system)) (get entity-component-types entity))))
             persistent!)))
-
-#_
-(defn kill-entity!
-    "Destroy an entity completely."
-    [entity]
-    (dosync
-        (let [component-types (get @entity-component-types entity)]
-            (alter all-entities disj entity)
-            (alter entity-component-types dissoc entity)
-            (doseq [type component-types]
-                (alter entity-components assoc type (dissoc (get @entity-components type) entity))))))
 
 (defn get-all-components-on-entity
     "Get all the components on a specific entity. Useful for debugging"
