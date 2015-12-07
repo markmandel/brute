723c03755245448268c03243e2c1f59fece3b876
Mark Mandel <markmandel@google.com>
2015-12-07 15:19:36 -0800

    A basic cljs build. Looks like it works(?)

--- project.clj
+++ project.clj
@@ -8,8 +8,14 @@
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :plugins [[lein-codox "0.9.0"]
             [lein-cljsbuild "1.1.1"]]
-
-  
+  :cljsbuild {:builds
+              [{:id           "src"
+                :source-paths ["src"]
+                :compiler     {:output-to     "target/js/brute.js"
+                               :output-dir    "target/js"
+                               :source-map    true
+                               :optimizations :none
+                               :pretty-print  true}}]}
 
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :source-paths ["dev"]
