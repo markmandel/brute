65101519fde1ecf1cdab5117a3ea048abc621fa4
Mark Mandel <mark.mandel@gmail.com>
2015-12-07 18:11:47 -0800

    Make it a map, easier to look at.

--- project.clj
+++ project.clj
@@ -8,21 +8,18 @@
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :plugins [[lein-codox "0.9.0"]
             [lein-cljsbuild "1.1.1"]]
-  :cljsbuild {:builds
-              [{:id           "src"
-                :source-paths ["src"]
-                :compiler     {:output-to     "target/js/src/brute.js"
-                               :output-dir    "target/js/src"
-                               :source-map    true
-                               :optimizations :none
-                               :pretty-print  true}}
-               {:id           "test"
-                :source-paths ["src" "test"]
-                :compiler     {:output-to     "target/js/test/brute.js"
-                               :output-dir    "target/js/test"
-                               :source-map    true
-                               :optimizations :none
-                               :pretty-print  true}}]}
+  :cljsbuild {:builds {:src  {:source-paths ["src"]
+                              :compiler     {:output-to     "target/js/src/brute.js"
+                                             :output-dir    "target/js/src"
+                                             :source-map    true
+                                             :optimizations :none
+                                             :pretty-print  true}}
+                       :test {:source-paths ["src" "test"]
+                              :compiler     {:output-to     "target/js/test/brute.js"
+                                             :output-dir    "target/js/test"
+                                             :source-map    true
+                                             :optimizations :none
+                                             :pretty-print  true}}}}
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :source-paths ["dev"]
                    :repl-options {:init-ns user}
